64887cf007d69da04dc2ca27b5fd4446c54b0ecf
amalakamala <amalakamala.a@gmail.com>
2017-08-16 12:14:29 -0300

    search 01

--- dist/js/script.js
+++ dist/js/script.js
@@ -10253,5 +10253,44 @@ return jQuery;
 } );
 
 $(document).ready(function() {
-	alert("hola");
+
+
+});
+$(document).ready(function() {
+
+	var cousine = "italian";
+	var apiKey = '6cc3db9f6f73c4a770e8ddde5284d1fb'; 
+	var entityId = '83';
+
+	var callbacksAjaxSearchItalian = function(){
+    	$.ajax({
+    		url:'https://developers.zomato.com/api/v2.1/search',
+      		type: 'GET',
+      		beforeSend: function(request) {
+				request.setRequestHeader("user-key", apiKey);
+			},
+      		dataType: 'json',
+      		data:{
+      			'entity_id' : entityId,
+      			'entity_type': 'city',
+      			'cuisines' : cousine
+      		}
+    	})
+    	.done(function(response){
+    		console.log(response);
+    		response.restaurants.forEach(function(e){
+    			console.log(e.restaurant); 
+    			console.log(e.restaurant.name); 
+    			$('.imprime').append(`<p>${e.restaurant.name}</p>`);
+    		});
+    	})
+    	.fail(function(error){
+    		console.log(error);
+                alert('Fail');
+        })
+        .always(function() {
+            console.log('OK');
+        })				
+	}
+	callbacksAjaxSearchItalian();
 });
