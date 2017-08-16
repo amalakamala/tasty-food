d512a95e3199cc71d159d25f8abc872a8835d8a3
VannPellegrini <vanessapellegrini@hotmail.com>
2017-08-16 13:02:43 -0300

    cambios js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
 
-	$('select').on('change',function(){
-		alert(this.value);
+	$('#select-city').on('change',function(){
+		var ciudad = "'"+ $('#select-city').val() +"'";
+		console.log(ciudad);
+		var cousine = "italian";
+		var apiKey = '6cc3db9f6f73c4a770e8ddde5284d1fb'; 
+		//var entityId = '83';
+
+		/*------- Callbacks -------*/
+	    	$.ajax({
+	    		url:'https://developers.zomato.com/api/v2.1/search',
+	      		type: 'GET',
+	      		beforeSend: function(request) {
+					request.setRequestHeader("user-key", apiKey);
+				},
+	      		dataType: 'json',
+	      		data:{
+	      			'entity_id' : ciudad,
+	      			'entity_type': 'city',
+	      			'cuisines' : cousine
+	      		}
+	    	})
+	    	.done(function(response){
+	    		//console.log(response);
+	    		response.restaurants.forEach(function(e){
+	    			//$('.imprime').empty();
+	    			//console.log(e.restaurant); 
+	    			//console.log(e.restaurant.name); 
+	    			$('.imprime').append(`<p>${e.restaurant.name}</p>`);
+	    		});
+	    	})
+	    	.fail(function(error){
+	    		//console.log(error);
+	            alert('Fail');
+	        })
+	        .always(function() {
+	            console.log('OK');
+	        })				
+
 	});
-
-	var cousine = "italian";
-	var apiKey = '6cc3db9f6f73c4a770e8ddde5284d1fb'; 
-	//var entityId = '83';
-
-	var callbacksAjaxSearchItalian = function(){
-    	$.ajax({
-    		url:'https://developers.zomato.com/api/v2.1/search',
-      		type: 'GET',
-      		beforeSend: function(request) {
-				request.setRequestHeader("user-key", apiKey);
-			},
-      		dataType: 'json',
-      		data:{
-      			'entity_id' : ciudad,
-      			'entity_type': 'city',
-      			'cuisines' : cousine
-      		}
-    	})
-    	.done(function(response){
-    		//console.log(response);
-    		response.restaurants.forEach(function(e){
-    			//console.log(e.restaurant); 
-    			//console.log(e.restaurant.name); 
-    			$('.imprime').append(`<p>${e.restaurant.name}</p>`);
-    		});
-    	})
-    	.fail(function(error){
-    		//console.log(error);
-            alert('Fail');
-        })
-        .always(function() {
-            console.log('OK');
-        })				
-	}
-	callbacksAjaxSearchItalian();
 });
