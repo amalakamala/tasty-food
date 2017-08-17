f99dc8b7e96e6557109f5d2ce55a1fc12649ff07
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-17 08:51:08 -0300

    Cambios de estilos

--- assets/js/cuenta.js
+++ assets/js/cuenta.js
@@ -1,47 +1,41 @@
 $(document).ready(function() {
-    $(".button-collapse").sideNav();
+    //$(".button-collapse").sideNav();
 	/*Validación de cuenta*/
 	var correo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 	var contra = /^[0-9]+(\.[0-9])?$/;
 
 	$(".crear").click(function(){			
 
-		var nombre = $("#name").val(); 
+		/*var nombre = $("#name").val(); 
 		var email = $("#email").val();
         var pass = $("#password").val();
 
         if(nombre == "" || nombre == contra){
-           $("#mensaje").fadeIn("slow");           
+           $(".mensaje").append('<div id="mensaje" class="errores">Ingrese nombre</div>');           
                return false;
         }else{
-            $("#mensaje").fadeOut();
+            $("#mensaje").remove();
             localStorage.setItem('nom', nombre);
             console.log(nombre);
         }
-
-        if(email == "" || !correo.test(email)){
-            $("#mensaje1").fadeIn("slow");
+        if(email == "" || correo.test(email) == false){
+            $(".mensaje1").append('<div id="mensaje1" class="errores">Ingrese correo</div>');
                return false;
         }else{
-            $("#mensaje1").fadeOut();
+            $("#mensaje1").remove();
             localStorage.setItem('correo', email);
             console.log(email);
         }
-
-        if(pass == "" || !contra.test(pass)){
-            $("#mensaje2").fadeIn("slow");
+        if(pass == "" || contra.test(pass) == false){
+            $(".mensaje2").append('<div id="mensaje2" class="errores">Ingrese sólo números</div>');
                return false;
         }else{
-            $("#mensaje2").fadeOut();
+            $("#mensaje2").remove();
             localStorage.setItem('contra', pass);
             console.log(pass);
         } 
 
-        return true; 
-
-        $("#name").val(""); 
-		$("#email").val("");
-        $("#password").val("");
+        return true; */
 	})
 
 	/*local storage*/
